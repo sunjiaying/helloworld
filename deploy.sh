17814a2361a75422f99998fdfe1c09e647135fe2
sunjiaying <sunjoin@qq.com>
2018-11-30 01:53:47 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ echo 'hello world.'
 cd /go/src/
 rm -rf github.com/sunjiaying/helloworld
 git clone https://github.com/sunjiaying/helloworld.git github.com/sunjiaying/helloworld
+cd /go/src/github.com/sunjiaying/helloworld/
+go build
 
 # curl https://raw.githubusercontent.com/sunjiaying/helloworld/master/deploy.sh | bash
 
