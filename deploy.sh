fcb4b75494630c4ad79402990307cfeceb043d99
sunjiaying <sunjoin@qq.com>
2018-12-17 09:50:31 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # echo 'hello world.'
 cd /go/src/
-git clone https://sunjoin@qq.com:123456@gitee.com/ods_development_team/ODS-APP-API.git ODS-APP-API_test
+git clone https://sunjoin:123456@gitee.com/ods_development_team/ODS-APP-API.git ODS-APP-API_test
 cd /go/src/ODS-APP-API_test
 go run main.go
 # cd /go/src/
